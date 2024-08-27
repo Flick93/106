3b89b616cb58320c6b037933881c39c4f26ad074
Flick93 <p.afflick@outlook.com>
2024-08-26 20:56:15 -0700

    third commit

--- task.js
+++ task.js
@@ -2,8 +2,8 @@ class Task {
 
     constructor (title, desc, color, date, status, budget){
     
-        this.title = title;
-    this.desctipton = desc;
+    this.title = title;
+    this.description = desc;
     this.color = color;
     this.date = date;
     this.status = status;
@@ -12,3 +12,4 @@ class Task {
     }
   
   }
+  //single responsibility principle
--- taskManager.js
+++ taskManager.js
@@ -1,37 +1,57 @@
-function saveTask(){
-    //get values
+function saveTask() {
+    // get values
     console.log("Task Manager...")
     const title = $("#txtTitle").val();
     const description = $("#txtDescription").val();
-    const color = $("#txtColore").val();
-    const Date = $("#txtDate").val();
-    const Status = $("#selStatus").val();
-    const Budget = $("#numBudget").val();
-
-    console.log(title, description, color, Date, Status, Budget);
-//creating an object
-    let taskSave = new Task(title, description, color, Date, Status, Budget);
-    console.log(taskSave)
-
-    //save to server
+    const color = $("#txtColor").val();
+    const date = $("#txtDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+  
+    //console.log(title, description, color, date, status, budget);
+  //creating an object
+    let taskSave = new Task(title, description, color, date, status, budget);
+    console.log(taskSave);
+  
+    // save to sever (post)
     $.ajax({
-
-        type: "post",
-        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
-        data: JSON.stringify(taskSave),
-        contentType: "application/json",
-        success: function (response) {
-          console.log(response)
-        },
-    
-        error: function (error) {
-          console.log(error)
-        }
-    })
-}
-
-function init(){
-    $("#btnSave").click(saveTask)
-}
-
-window.onload = init
+      type: "post",
+      url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+      data: JSON.stringify(taskSave),
+      contentType: "application/json",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+    displayTask(taskSave);
+  }
+  //display from server (get)
+  function displayTask(task){
+    let syntax = `
+    <div class="task-container" style="border-color:${task.color}">
+      <div class="task">
+        <div class="info">
+          <h5>${task.title}</h5>
+          <p>${task.description}</p>
+        </div>   
+        <div class="status">${task.status}</div>
+        <div class="date-budget">
+          <span>${task.date}</span>
+          <span>${task.budget}</span>
+        </div>
+      </div>  
+    </div>
+    `;
+  
+    $("#list").append(syntax);
+  }
+  
+  
+  function init() {
+    $("#btnSave").click(saveTask);
+  }
+  
+  window.onload = init
